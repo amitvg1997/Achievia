62618b26d55d01a11c246144c1185a338bcaf888
NiranjanaRajaraman <150949193+NiranjanaRajaraman@users.noreply.github.com>
2025-12-11 14:58:00 +0000

    testChange

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ android {
         minSdk = 25
         targetSdk = 36
         versionCode = 1
+        
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
